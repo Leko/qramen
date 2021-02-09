073af6880f5b8678646b88bf72ccc8431f2c7aa5
Leko <leko.noor@gmail.com>
2021-02-09 12:42:49 +0800

    Send WebVitals

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import { Metric } from 'web-vitals'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import './index.css'
 
 ReactDOM.render(
   <StrictMode>
@@ -11,7 +12,14 @@ ReactDOM.render(
   document.getElementById('root')
 )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+function sendToAnalytics({ id, name, value }: Metric) {
+  // @ts-expect-error ga is defined globally
+  ga('send', 'event', {
+    eventCategory: 'Web Vitals',
+    eventAction: name,
+    eventValue: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
+    eventLabel: id, // id unique to current page load
+    nonInteraction: true, // avoids affecting bounce rate
+  })
+}
+reportWebVitals(sendToAnalytics)
