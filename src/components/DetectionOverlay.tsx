32e8a83ba50393055b384b188857c9c208fd89e0
Leko <leko.noor@gmail.com>
2021-02-12 20:51:52 +0800

    Draw guide

--- src/components/DetectionOverlay.tsx
+++ src/components/DetectionOverlay.tsx
@@ -8,6 +8,43 @@ interface Props {
   barcodes: DetectedBarcode[]
 }
 
+function getCornerCoordinates(
+  width: number,
+  height: number
+): [number, number, number, number, number, number][] {
+  const size = Math.min(width, height) - 160
+  const cornerSize = size / 8
+  const guideX = width / 2 - size / 2
+  const guideY = height / 2 - size / 2
+  return [
+    [guideX, guideY + cornerSize, guideX, guideY, guideX + cornerSize, guideY],
+    [
+      guideX + size - cornerSize,
+      guideY,
+      guideX + size,
+      guideY,
+      guideX + size,
+      guideY + cornerSize,
+    ],
+    [
+      guideX + size,
+      guideY + size - cornerSize,
+      guideX + size,
+      guideY + size,
+      guideX + size - cornerSize,
+      guideY + size,
+    ],
+    [
+      guideX + cornerSize,
+      guideY + size,
+      guideX,
+      guideY + size,
+      guideX,
+      guideY + size - cornerSize,
+    ],
+  ]
+}
+
 export function DetectionOverlay(props: Props) {
   const { barcodes, width, height } = props
   const { ctx, canvasRef } = useCanvasContext()
@@ -27,6 +64,17 @@ export function DetectionOverlay(props: Props) {
 
     requestAnimationFrame(() => {
       ctx.clearRect(0, 0, width, height)
+      for (const [sx, sy, cpx, cpy, x, y] of getCornerCoordinates(
+        width,
+        height
+      )) {
+        ctx.beginPath()
+        ctx.moveTo(sx, sy)
+        ctx.quadraticCurveTo(cpx, cpy, x, y)
+        ctx.strokeStyle = 'white'
+        ctx.lineWidth = 2
+        ctx.stroke()
+      }
       for (const barcode of barcodes) {
         const start = barcode.cornerPoints[0]
         ctx.beginPath()
@@ -36,7 +84,7 @@ export function DetectionOverlay(props: Props) {
         }
         ctx.closePath()
         ctx.strokeStyle = barcode.hashColor
-        ctx.lineWidth = 5
+        ctx.lineWidth = 4
         ctx.stroke()
       }
     })
