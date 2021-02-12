152cc2bcc3f1b96aa0f2b4dcf7c1607a9e059d74
Leko <leko.noor@gmail.com>
2021-02-12 20:28:36 +0800

    Improve instruction

--- src/components/Instruction.tsx
+++ src/components/Instruction.tsx
@@ -4,8 +4,20 @@ export function Instruction() {
   return (
     <aside className="instruction-help-wrap">
       <div className="instruction">
-        <h2 className="instruction-title">Scan QR codes</h2>
-        <p>Scan the QR code and the result will be displayed here.</p>
+        <div>
+          {/* @ts-ignore */}
+          <lottie-player
+            src="https://assets10.lottiefiles.com/packages/lf20_VBwuuR.json"
+            background="transparent"
+            style={{ width: 120, height: 120 }}
+            loop
+            autoplay
+          />
+        </div>
+        <div className="instruction-pane">
+          <h2 className="instruction-title">Scan QR codes</h2>
+          <p>Scan the QR code and the result will be displayed here.</p>
+        </div>
       </div>
     </aside>
   )
